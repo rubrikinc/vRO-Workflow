f28f4034efbff400a7bf873293d6346cf2bd3312
Matt Altimar <mattaltimar@users.noreply.github.com>
2016-05-04 12:16:44 -0400

    Create rubrikRefreshvCenter.js

---
+++ rubrikRefreshvCenter.js
@@ -0,0 +1,55 @@
+// vRO Action rubrikRefreshvCenter from package com.rubrik.library.rest.package
+//
+// NAME             - TYPE                - DESCRIPTION
+// ----------------------------------------------------
+// <INPUT PARAMETERS> 
+// tokenBase64      - string              - Rubrik authentication token
+// rubrikHost       - REST:REST Host      - Rubrik REST host
+// vCenterId        - string              - Rubrik vCenter ID
+//
+// <RETURN VALUE>
+// N/A              - string              - Job ID of Refresh Job
+
+//Construct REST call
+var method = "POST";
+var url = "internal/job/type/refresh";
+var content = '{"vCenterId":"' + vCenterId + '"}';
+var request = rubrikHost.createRequest(method, url, content);
+var token = ("Basic " + tokenBase64);
+request.contentType = "application\/json";
+request.setHeader("Accept", "application/json");
+request.setHeader("Authorization", token);
+
+//Log REST Call Info
+System.log("token = " + token);
+System.log("REST URL = " + request.fullUrl);
+System.log("REST Content = " + content);
+
+//Execute REST Call
+try {
+	var response = request.execute();
+}
+catch (ex) {
+	System.error(ex);
+	throw "Stopping Execution";
+}
+
+//Evaluate REST Response
+var statusCode = response.statusCode;
+if (statusCode == 200) {
+	System.log("REST Execution Successful");
+}
+else {
+	System.log("ERROR Executing REST operation");
+	System.error("Status Code = " + statusCode);
+	throw "Stopping Execution";
+}
+
+//Log Response
+
+//Bugfix - need to remove quotes around job ID
+jobId = response.contentAsString.substring(1,response.contentAsString.length - 1)
+System.log("Job ID: " + jobId);
+
+//Return Job ID
+return jobId;
