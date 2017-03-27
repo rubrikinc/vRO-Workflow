0c25fd96a4bb40ecedec843bf587cf727cf8f9e2
Matt Altimar <mattaltimar@users.noreply.github.com>
2017-03-27 08:49:06 -0400

    Update rubrikGetSnapshotID.js

--- rubrikGetSnapshotID.js
+++ rubrikGetSnapshotID.js
@@ -1,19 +1,20 @@
-// vRO Action rubrikGetSnapshotID from package com.rubrik.library.rest.package
+// vRO Action rubrikGetSLAID from package com.rubrik.library.rest.package
 //
 // NAME             - TYPE                - DESCRIPTION
 // ----------------------------------------------------
 // <INPUT PARAMETERS> 
 // tokenBase64      - string              - Rubrik authentication token
 // rubrikHost       - REST:REST Host      - Rubrik REST host
-// vmId             - string              - Rubrik VM ID
-// beforeDate	    - Date                - Date to get snapshot before
+// slaDomainName    - string              - SLA Domain Name
+// query_limit		- number			  - Query limit
+// api_url			- string			  - API URL
 //
 // <RETURN VALUE>
-// N/A              - string              - Snapshot ID
+// N/A              - string              - SLA Domain ID
 
 //Construct REST call
 var method = "GET";
-var url = "snapshot?vm=" + vmId;
+var url = api_url + "sla_domain?limit=" + query_limit;
 var content = null;
 var request = rubrikHost.createRequest(method, url, content);
 var token = ("Basic " + tokenBase64);
@@ -46,43 +47,17 @@ else {
 	throw "Stopping Execution";
 }
 
-//Loop Through Snapshots Looking for the first one that we have before the provided date
-var json = JSON.parse(response.contentAsString);
-
-//need to convert requested date to GMT which is what is stored with Rubrik snapshots
-var gmtDate = new Date(beforeDate.valueOf() + beforeDate.getTimezoneOffset() * 60000);
+//Loop Through SLA Domains to Find Corresponding ID
+var json = (JSON.parse(response.contentAsString)).data;
 
 for(var i = 0; i < json.length; i++) {
 	var obj = json[i];
-
-	//create a date object from the date string returned by Rubrik
-	var rubrikDate = obj.date;
-	var rubrikYear = rubrikDate[0] + rubrikDate[1] + rubrikDate[2] + rubrikDate[3];
-	var rubrikMonth = rubrikDate[5] + (rubrikDate[6]-1);
-	var rubrikDay = rubrikDate[8] + rubrikDate[9];
-	var rubrikHour = rubrikDate[11] + rubrikDate[12];
-	var rubrikMin = rubrikDate[14] + rubrikDate[15];
-	var rubrikSec = rubrikDate[17] + rubrikDate[18];
-	
-	var rDate = new Date();
-	rDate.setYear(rubrikYear);
-	rDate.setMonth(rubrikMonth);
-	rDate.setDate(rubrikDay);
-	rDate.setHours(rubrikHour);
-	rDate.setMinutes(rubrikMin);
-	rDate.setSeconds(rubrikSec);
-	
-	//if date is before requested date then set this as the snapshot to restore
-	if(rDate.getTime() <= gmtDate.getTime()) {
-		var snapshotId = obj.id;
-		System.log("***SNAPSHOT FOUND***");
-		System.log("Snapshot ID = " + snapshotId);
-		System.log("Snapshot date = " + obj.date);
-		return(snapshotId);
+	if(obj.name == slaDomainName) {
+		System.log("SLA DOMAIN ID = " + obj.id);
+		return obj.id;
 	}
 }
 
-
-//Snapshot Not Found
-System.error("No Snapshot Found Before Date Provided");
+//SLA ID Not Found
+System.error("No Matching SLA Domain Found With the Name: " + slaDomainName);
 throw "Stopping Execution";
